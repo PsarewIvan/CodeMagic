5c5c950401e6a36d1dc96db3ab9f764439924730
Ivan Psarev <psarew.ivan@yandex.ru>
2019-08-21 21:29:51 +0300

    Поправил синтаксис, убрал лишнее

--- js/stat.js
+++ js/stat.js
@@ -20,50 +20,46 @@ var playersNamesOffset = 15;
 var playersValuesOffset = 10;
 
 var renderCloud = function(ctx, cloudCoord) {
-    ctx.fillStyle = CLOUD_SHADOW_FILL;
-    ctx.fillRect(cloudCoord[0] + SHADOW_OFFSET, cloudCoord[1] + SHADOW_OFFSET, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillStyle = CLOUD_SHADOW_FILL;
+  ctx.fillRect(cloudCoord[0] + SHADOW_OFFSET, cloudCoord[1] + SHADOW_OFFSET, CLOUD_WIDTH, CLOUD_HEIGHT);
 
-    ctx.fillStyle = CLOUD_FILL;
-    ctx.fillRect(cloudCoord[0], cloudCoord[1], CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillStyle = CLOUD_FILL;
+  ctx.fillRect(cloudCoord[0], cloudCoord[1], CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
 var getMaxValue = function(times) {
-    var maxValue = times[0];
-    for (var i = 0; i < times.length; i++) {
-       if (times[i] > maxValue) {
-           (maxValue = times[i]);
-       };
-    }
-    return maxValue;
+  var maxValue = times[0];
+  for (var i = 0; i < times.length; i++) {
+    if (times[i] > maxValue) {
+      (maxValue = times[i]);
+    };
+  }
+  return maxValue;
 };
 
 var getRandomBlueColorHSL = function() {
-    var randomSaturation = Math.floor(Math.random() * 101);
-    return 'hsl(240, ' + randomSaturation + '%, 50%)';
-}
+  var randomSaturation = Math.floor(Math.random() * 101);
+  return 'hsl(240, ' + randomSaturation + '%, 50%)';
+};
 
 window.renderStatistics = function(ctx, names, times) {
-    renderCloud(ctx, cloudStartCoord);
+  renderCloud(ctx, cloudStartCoord);
 
-    ctx.fillStyle = textColor;
-    ctx.font = textFontStyle;
-    ctx.fillText('Ура вы победили!', textStartCoord[0], textStartCoord[1]);
-    ctx.fillText('Список результатов:', textStartCoord[0], textStartCoord[1] + textVerticalOffset);
+  ctx.fillStyle = textColor;
+  ctx.font = textFontStyle;
+  ctx.fillText('Ура вы победили!', textStartCoord[0], textStartCoord[1]);
+  ctx.fillText('Список результатов:', textStartCoord[0], textStartCoord[1] + textVerticalOffset);
 
-    var heightRatio = getMaxValue(times) / BAR_HEIGHT;
-    for (var i = 0; i < names.length; i++) {
-        var barWidth = barStartCoord[0] + i * (BAR_WIDTH + barHorizontalOffset);
-        var barHeight = -Math.floor(times[i] / heightRatio);
+  var heightRatio = getMaxValue(times) / BAR_HEIGHT;
+  for (var i = 0; i < names.length; i++) {
+    var barWidth = barStartCoord[0] + i * (BAR_WIDTH + barHorizontalOffset);
+    var barHeight = -Math.floor(times[i] / heightRatio);
 
-        ctx.fillStyle = (i === 0) ? PLAYER_BAR_COLOR : getRandomBlueColorHSL();
-        ctx.fillRect(barWidth, barStartCoord[1], BAR_WIDTH, barHeight);
+    ctx.fillStyle = (i === 0) ? PLAYER_BAR_COLOR : getRandomBlueColorHSL();
+    ctx.fillRect(barWidth, barStartCoord[1], BAR_WIDTH, barHeight);
 
-        ctx.fillStyle = '#000';
-        ctx.fillText(names[i], barWidth, barStartCoord[1] + playersNamesOffset);
-        ctx.fillText(Math.floor(times[i]), barWidth, barStartCoord[1] + barHeight - playersValuesOffset);
-    }
-
-    this.console.log(getRandomBlueColorHSL());
-    this.console.log(names);
-    this.console.log(times);
+    ctx.fillStyle = '#000';
+    ctx.fillText(names[i], barWidth, barStartCoord[1] + playersNamesOffset);
+    ctx.fillText(Math.floor(times[i]), barWidth, barStartCoord[1] + barHeight - playersValuesOffset);
+  }
 };
